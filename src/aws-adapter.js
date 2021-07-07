e25892bf4b7a47dd0c8d232c0c6cdd5ab852cc71
Dominique Pfister <dominique.pfister@gmail.com>
2021-07-07 11:34:11 +0200

    fix: support delivery failure (#44)
    * fix: support delivery failure
    
    * chore: make epsagon avoidance clear
    
    * fix: remove duplicate condition
    
    * fix: use epsagon unconditionally

--- src/aws-adapter.js
+++ src/aws-adapter.js
@@ -115,6 +115,10 @@ async function lambdaAdapter(event, context, secrets = {}) {
     }
     // eslint-disable-next-line no-console
     console.error('error while invoking function', e);
+    if (event.nonHttp) {
+      // let caller see the exception thrown
+      throw e;
+    }
     return {
       statusCode: 500,
       headers: {
@@ -135,8 +139,11 @@ async function lambdaAdapter(event, context, secrets = {}) {
  */
 async function lambda(evt, ctx) {
   try {
+    evt.nonHttp = (!evt.requestContext);
+
     const secrets = await getAWSSecrets(ctx.functionName);
     let handler = (event, context) => lambdaAdapter(event, context, secrets);
+
     if (secrets.EPSAGON_TOKEN) {
       // check if health check
       const suffix = evt.pathParameters && evt.pathParameters.path ? `/${evt.pathParameters.path}` : '';
@@ -146,8 +153,8 @@ async function lambda(evt, ctx) {
         });
       }
     }
-    if (!evt.requestContext) {
-      // function was invoked directly, not through API Gateway
+    if (evt.nonHttp) {
+      // mimic minimal requirements for our environment setup in lambdaAdapter
       const searchParams = new URLSearchParams();
       Object.getOwnPropertyNames(evt).forEach((name) => {
         const value = evt[name];
@@ -162,6 +169,10 @@ async function lambda(evt, ctx) {
     }
     return handler(evt, ctx);
   } catch (e) {
+    if (evt.nonHttp) {
+      // let caller see the exception thrown
+      throw e;
+    }
     return {
       statusCode: e.statusCode || 500,
       headers: {
