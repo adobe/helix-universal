df882a4bfc2e4bb2ff624d06df8f2398d6ff0e76
Tobias Bocanegra <tripod@adobe.com>
2023-01-30 17:48:11 +0100

    feat: support factory for main function

--- src/google-adapter.js
+++ src/google-adapter.js
@@ -24,85 +24,100 @@ import { GoogleResolver } from './resolver.js';
 import { GoogleStorage } from './google-storage.js';
 
 /**
- * Universal adapter for google cloud functions.
- * @param {*} req express request
- * @param {*} res express response
+ * Creates a universal adapter for google cloud functions.
+ * @param {function} opts.factory the factory for the main function. defaults to dynamic import
+ * @param {object} opts options
  */
-async function googleAdapter(req, res) {
-  try {
-    const request = new Request(`https://${req.hostname}/${process.env.K_SERVICE}${req.originalUrl}`, {
-      method: req.method,
-      headers: req.headers,
-      // google magically does the right thing here
-      body: req.rawBody,
-    });
-
-    const [subdomain] = req.headers.host.split('.');
-    const [country, region, ...servicename] = subdomain.split('-');
-    const [packageName, name] = process.env.K_SERVICE.split('--');
-
-    const context = {
-      resolver: new GoogleResolver(req),
-      pathInfo: {
-        // original: /foo?hey=bar
-        suffix: req.originalUrl.replace(/\?.*/, ''),
-      },
-      runtime: {
-        name: 'googlecloud-functions',
-        region: `${country}-${region}`,
-      },
-      func: {
-        name,
-        package: packageName,
-        version: process.env.K_REVISION,
-        fqn: process.env.K_SERVICE,
-        app: servicename.join('-'),
-      },
-      invocation: {
-        id: req.headers['function-execution-id'],
-        deadline: Number.parseInt(req.headers['x-appengine-timeout-ms'], 10) + Date.now(),
-        transactionId: request.headers.get('x-transaction-id'),
-        requestId: request.headers.get('x-cloud-trace-context'),
-      },
-      env: {
-        ...process.env,
-      },
-      log: createDefaultLogger(),
-      storage: GoogleStorage,
-    };
-
-    updateProcessEnv(context);
-    const { main } = await import('./main.js');
-
-    const response = await main(request, context);
-    ensureUTF8Charset(response);
-    ensureInvocationId(response, context);
-
-    const body = isBinary(response.headers)
-      ? Buffer.from(await response.arrayBuffer())
-      : await response.text();
-    Array.from(response.headers.entries())
-      .reduce((r, [header, value]) => r.set(header, value), res.status(response.status))
-      .send(body);
-  } catch (e) {
-    if (e instanceof TypeError && e.code === 'ERR_INVALID_CHAR') {
-      // eslint-disable-next-line no-console
-      console.error('invalid request header', e.message);
-      res.status(400).send(e.message);
-      return;
-    }
-    // eslint-disable-next-line no-console
-    console.error('error while invoking function', e);
-    res
-      .status(500)
-      .set('content-type', 'text/plain')
-      .set('x-invocation-id', req.headers['function-execution-id'])
-      .set('x-error', cleanupHeaderValue(e.message))
-      .send(e.message);
+export function createAdapter(opts = {}) {
+  let { factory } = opts;
+  if (!factory) {
+    factory = async () => (await import('./main.js')).main;
   }
+
+  /**
+   * Universal adapter for google cloud functions.
+   * @param {*} req express request
+   * @param {*} res express response
+   */
+  return async function googleAdapter(req, res) {
+    try {
+      const request = new Request(`https://${req.hostname}/${process.env.K_SERVICE}${req.originalUrl}`, {
+        method: req.method,
+        headers: req.headers,
+        // google magically does the right thing here
+        body: req.rawBody,
+      });
+
+      const [subdomain] = req.headers.host.split('.');
+      const [country, region, ...servicename] = subdomain.split('-');
+      const [packageName, name] = process.env.K_SERVICE.split('--');
+
+      const context = {
+        resolver: new GoogleResolver(req),
+        pathInfo: {
+          // original: /foo?hey=bar
+          suffix: req.originalUrl.replace(/\?.*/, ''),
+        },
+        runtime: {
+          name: 'googlecloud-functions',
+          region: `${country}-${region}`,
+        },
+        func: {
+          name,
+          package: packageName,
+          version: process.env.K_REVISION,
+          fqn: process.env.K_SERVICE,
+          app: servicename.join('-'),
+        },
+        invocation: {
+          id: req.headers['function-execution-id'],
+          deadline: Number.parseInt(req.headers['x-appengine-timeout-ms'], 10) + Date.now(),
+          transactionId: request.headers.get('x-transaction-id'),
+          requestId: request.headers.get('x-cloud-trace-context'),
+        },
+        env: {
+          ...process.env,
+        },
+        log: createDefaultLogger(),
+        storage: GoogleStorage,
+      };
+
+      updateProcessEnv(context);
+      // create the `main` after the process.env is set. this gives the functions the ability to
+      // use process.env in their global scopes.
+      const main = await factory();
+
+      const response = await main(request, context);
+      ensureUTF8Charset(response);
+      ensureInvocationId(response, context);
+
+      const body = isBinary(response.headers)
+        ? Buffer.from(await response.arrayBuffer())
+        : await response.text();
+      Array.from(response.headers.entries())
+        .reduce((r, [header, value]) => r.set(header, value), res.status(response.status))
+        .send(body);
+    } catch (e) {
+      if (e instanceof TypeError && e.code === 'ERR_INVALID_CHAR') {
+        // eslint-disable-next-line no-console
+        console.error('invalid request header', e.message);
+        res.status(400).send(e.message);
+        return;
+      }
+      // eslint-disable-next-line no-console
+      console.error('error while invoking function', e);
+      res
+        .status(500)
+        .set('content-type', 'text/plain')
+        .set('x-invocation-id', req.headers['function-execution-id'])
+        .set('x-error', cleanupHeaderValue(e.message))
+        .send(e.message);
+    }
+  };
 }
 
-function wrap(adapter) {
+// export 'wrap' so it can be used like: `google.wrap(google.raw).with(epsagon).with(secrets);
+export function wrap(adapter) {
   /**
    * Universal adapter for google cloud functions.
    * @param {*} req express request
@@ -132,7 +147,4 @@ function wrap(adapter) {
 }
 
 // default export contains the aws secrets plugin
-export const google = wrap(googleAdapter).with(googleSecretsPlugin);
-// export 'wrap' so it can be used like: `google.wrap(google.raw).with(epsagon).with(secrets);
-google.wrap = wrap;
-google.raw = googleAdapter;
+export const google = wrap(createAdapter()).with(googleSecretsPlugin);
